149fb5a42d4356cf5a0978a532ac85319155a181
quietpochatok <bosroin@gmail.com>
2020-12-21 22:36:05 +0500

    add test for 'current_game_question' for model Game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -115,4 +115,12 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq :money
     end
   end
+
+  # тест на возвращение вопроса, что не отвечен.
+  describe '#current_game_question' do
+    it 'return question without answer' do
+      expect(game_w_questions.current_game_question).
+        to eq game_w_questions.game_questions.first
+    end
+  end
 end
