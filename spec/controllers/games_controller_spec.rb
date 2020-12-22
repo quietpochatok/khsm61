dc598ccea05311c1a7143c5915c9189b97d305c5
quietpochatok <bosroin@gmail.com>
2020-12-23 00:33:37 +0500

    add test for GamesController: anonymous user/logged in user

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe GamesController, type: :controller do
     # Аноним не может смотреть игру
     it 'kicks from #show' do
       # Вызываем экшен
-      get :show, params: {id: game_w_questions.id}
+      get :show, id: game_w_questions.id
       # Проверяем ответ
       # статус ответа не равен 200
       expect(response.status).not_to eq(200)
@@ -22,6 +22,68 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       # Во flash должно быть сообщение об ошибке
       expect(flash[:alert]).to be
+    end
+  end
+
+  # группа тестов на экшены контроллера, доступных залогиненным юзерам
+  context 'Usual user' do
+    # перед каждым тестом в группе
+    before(:each) { sign_in user } # логиним юзера user с помощью спец. Devise метода sign_in
+
+    it 'creates game' do
+      # Создадим пачку вопросов
+      generate_questions(15)
+
+      # Экшен create у нас отвечает на запрос POST
+      post :create
+
+      # Вытаскиваем из контроллера поле @game
+      game = assigns(:game)
+
+      # Проверяем состояние этой игры: она не закончена
+      # Юзер должен быть именно тот, которого залогинили
+      expect(game.finished?).to be false
+      # У игры назначен юзер
+      expect(game.user).to eq(user)
+
+      # Проверяем, есть ли редирект на страницу этой игры
+      # И есть ли сообщение об этом
+      expect(response).to redirect_to(game_path(game))
+      expect(flash[:notice]).to be
+    end
+
+    it '#show game' do
+      # Показываем по GET-запросу
+      get :show, id: game_w_questions.id
+      # Вытаскиваем из контроллера поле @game
+      game = assigns(:game)
+      # Игра не закончена
+      expect(game.finished?).to be_falsey
+      # Юзер именно тот, которого залогинили
+      expect(game.user).to eq(user)
+
+      # Проверяем статус ответа (200 ОК)
+      expect(response.status).to eq(200)
+      # Проверяем рендерится ли шаблон show (НЕ сам шаблон!)
+      expect(response).to render_template('show')
+    end
+
+    # Тест проверяет поведение контроллера, если юзер ответил на вопрос верно
+    it 'answers correct' do
+      # Дёргаем экшен answer, передаем параметр params[:letter]
+      put :answer, id: game_w_questions.id, letter: game_w_questions.current_game_question.correct_answer_key
+      # Вытаскиваем из контроллера поле @game
+      game = assigns(:game)
+
+      # Игра не закончена
+      expect(game.finished?).to be_falsey
+      # Уровень больше 0
+      expect(game.current_level).to be > 0
+
+      # Редирект на страницу игры
+      expect(response).to redirect_to(game_path(game))
+      # Флеш пустой
+      expect(flash.empty?).to be_truthy # удачный ответ не заполняет flash
     end
   end
 end
