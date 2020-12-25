f407ec45f5f8c5e9c72fd7dd2e7dffe1bdb4f8dd
quietpochatok <bosroin@gmail.com>
2020-12-26 02:23:22 +0500

    Added feature the 'Hints': migrations / tests for them in the GameQuestion model; Added GameHelpGenerator class

--- app/models/game_question.rb
+++ app/models/game_question.rb
@@ -1,3 +1,4 @@
+require 'game_help_generator'
 #  (c) goodprogrammer.ru
 #
 # Игровой вопрос — модель, которая связывает игру и вопрос. При создании новой
@@ -28,6 +29,24 @@ class GameQuestion < ActiveRecord::Base
   # В полях a, b, c и d прячутся индексы ответов из объекта :game. Каждый из
   # них — целое число от 1 до 4.
   validates :a, :b, :c, :d, inclusion: {in: 1..4}
+
+  # Автоматическая сериализация поля в базу (мы юзаем как обычный хэш, а
+  # рельсы в базе хранят как строчку).
+  #
+  # См. ссылки в материалах урока
+  serialize :help_hash, Hash
+
+  # help_hash у нас имеет такой формат:
+  # {
+  #   # При использовании подсказски остались варианты a и b
+  #   fifty_fifty: ['a', 'b'],
+  #
+  #   # Распределение голосов по вариантам a, b, c, d
+  #   audience_help: {'a' => 42, 'c' => 37 ...},
+  #
+  #   # Друг решил, что правильный ответ А (просто пишем текстом)
+  #   friend_call: 'Василий Петрович считает, что правильный ответ A'
+  # }
 
   # Основные методы для доступа к данным в шаблонах и контроллерах:
 
@@ -65,4 +84,61 @@ class GameQuestion < ActiveRecord::Base
   def correct_answer
     variants[correct_answer_key]
   end
+
+  # Добавляем в help_hash по ключю fifty_fifty — массив из двух вариантов:
+  # правильный и случайный и сохраняем объект.
+  def add_fifty_fifty
+    self.help_hash[:fifty_fifty] = [
+      correct_answer_key,
+      (%w(a b c d) - [correct_answer_key]).sample
+    ]
+
+    save
+  end
+
+  #Генерируем в help_hash случайное распределение по вариантам и сохраняем объект
+  def add_audience_help
+    # Массив ключей
+    keys_to_use = keys_to_use_in_help
+
+    self.help_hash[:audience_help] =
+      GameHelpGenerator.audience_distribution(keys_to_use, correct_answer_key)
+
+    save
+  end
+
+  # Добавляем в help_hash подсказку друга и сохраняем объект
+  def add_friend_call
+    # Массив ключей
+    keys_to_use = keys_to_use_in_help
+
+    self.help_hash[:friend_call] =
+      GameHelpGenerator.friend_call(keys_to_use, correct_answer_key)
+
+    save
+  end
+
+  def apply_help!(help_type)
+    case help_type.to_sym
+    when :fifty_fifty
+      add_fifty_fifty
+    when :audience_help
+      add_audience_help
+    when :friend_call
+      add_friend_call
+    end
+  end
+
+  private
+
+  # Рассчитываем какие ключи нам доступны в подсказках
+  def keys_to_use_in_help
+    keys_to_use = variants.keys
+
+    # Учитываем наличие подсказки 50/50
+    keys_to_use = help_hash[:fifty_fifty] if help_hash.has_key?(:fifty_fifty)
+
+    keys_to_use
+  end
+
 end
--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -42,6 +42,92 @@ RSpec.describe GameQuestion, type: :model do
       end
     end
   end
+
+  # help_hash у нас имеет такой формат:
+  # {
+  #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
+  #   audience_help: {'a' => 42, 'c' => 37 ...}, # Распределение голосов по вариантам a, b, c, d
+  #   friend_call: 'Василий Петрович считает, что правильный ответ A'
+  # }
+
+  context 'user helpers' do
+    it 'correct audience_help' do
+      expect(game_question.help_hash).not_to include(:audience_help)
+
+      game_question.add_audience_help
+
+      expect(game_question.help_hash).to include(:audience_help)
+
+      audience_help_value = game_question.help_hash[:audience_help]
+      expect(audience_help_value.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    it 'correct friend_call' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      # вызовем подсказку
+      game_question.add_friend_call
+
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:friend_call)
+      friend_call_value = game_question.help_hash[:friend_call]
+
+      # проверяем наличие текста, а также вариант ответа.
+      #expect(friend_call_value).to include(GameHelpGenerator.friend_call(['a', 'b', 'c', 'd'], game_question.correct_answer_key))
+      expect(friend_call_value).to include('считает, что это вариант')
+      expect(friend_call_value).to match(/[ABCD]+/)
+    end
+
+    # проверяем работу подсказкки 50/50
+    it 'correct fifty_fifty' do
+      # сначала убедимся, в подсказках пока нет нужного ключа
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+
+      # вызовем подсказку
+      game_question.add_fifty_fifty
+
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:fifty_fifty)
+      fifty_fifty_value = game_question.help_hash[:fifty_fifty]
+
+      # должен быть и остаться правильный ответ
+      # для наших тестов формально это b
+      expect(fifty_fifty_value).to include('b')
+      # всего должно остаться 2 варианта
+      expect(fifty_fifty_value.size).to eq(2)
+    end
+
+    # it 'correct .help_hash' do
+    #   # на фабрике у нас изначально хэш пустой
+    #   expect(game_question.help_hash).to be({})
+    #
+    #   game_question.help_hash[:fifty_fifty] = %w[a b]
+    #   expect(game_question.save).to be_truthy
+    #   gq_check = GameQuestion.find(game_question.id)
+    #   expect(gq_check.help_hash).to eq( { fifty_fifty: %w[a b] } )
+    #  !
+    #   !выкидывает забавный эксепшон при прогонке, спросить у менторов
+    # !
+    # end
+
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
+  end
 end
 
 
