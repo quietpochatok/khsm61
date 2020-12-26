2c1f5e47d613c78ce7c1a620974e120431e2feed
quietpochatok <bosroin@gmail.com>
2020-12-26 21:41:05 +0500

    Add views for hints & test for views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
+    @users = User.all.order(balance: :desc)
   end
 
   def show
+    # Все игры пользователя в порядке давности — от самой новой к старой
+    @games = @user.games.order(created_at: :desc)
   end
 
   private
